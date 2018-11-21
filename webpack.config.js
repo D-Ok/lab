4eeded0c261db264f24fab6c216da2b1ecf472ea
Daryna Oliinyk <darusya172@gmail.com>
2018-11-21 13:25:08 +0200

    full bable addition

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,27 @@ module.exports = {
         filename: 'js/main.js',
     },
 };
+
+// ... contents of webpack.config.js
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'js/main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            // ...additional rules...
+        ],
+    },
+};
